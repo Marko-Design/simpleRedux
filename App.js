92212932aa003d8f9d902637ecd427fdd2749b61
Marko Asanovic <159731517+Marko-Design@users.noreply.github.com>
2024-02-12 02:38:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Posts from "./Posts/Posts";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Posts />
+      </header>
+    </div>
+  );
+}
+
+export default App;
